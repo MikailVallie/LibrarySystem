e028aa5796b3f0247ca67cbbb4b443cc7b1dd572
Mika'ilVallie <mikailvallie6@gmail.com>
2025-03-28 15:13:28 +0200

    commit all files to master

---
+++ src/main/java/za/ac/cput/domain/Book.java
@@ -0,0 +1,10 @@
+package za.ac.cput.domain;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean availability;
+
+}
